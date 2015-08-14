cb3ba182d257c71bc783e9f5fb1725ec976e128e
Your Name <you@example.com>
2015-08-14 14:09:01 +0100

    removing menu

--- app/src/main/java/mimecast/com/recruitment/application/CancellableSortActivity.java
+++ app/src/main/java/mimecast/com/recruitment/application/CancellableSortActivity.java
@@ -2,12 +2,9 @@ package mimecast.com.recruitment.application;
 
 import android.app.Activity;
 import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -27,40 +24,17 @@ import mimecast.com.recruitment.task.EmailHeaderListReverseChronologicalSortTask
  * Created by Michael Aubert on 27/02/2015.
  */
 
-public class CancellableSortActivity extends ActionBarActivity {
+public class CancellableSortActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cancellable_sort);
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
+            getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_cancellable_sort, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**
