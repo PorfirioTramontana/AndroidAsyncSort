809b7cc6ad702c8a934243d1e6b8467bcac69941
Porfirio Tramontana <porfirio.tramontana@gmail.com>
2018-05-28 19:43:22 +0200

    TEST zero e primo test

---
+++ app/src/androidTest/java/mimecast/com/recruitment/UIAutomatorAsyncTaskTest.java
@@ -0,0 +1,206 @@
+package mimecast.com.recruitment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.RemoteException;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.support.test.uiautomator.Until;
+import android.util.Log;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.Semaphore;
+
+import mimecast.com.recruitment.task.DownloadEmailHeaderListTask;
+import mimecast.com.recruitment.task.EmailHeaderListReverseChronologicalSortTask;
+
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
+public class UIAutomatorAsyncTaskTest {
+
+    private static final String BASIC_SAMPLE_PACKAGE = "mimecast.com.recruitment";
+    private static final int LAUNCH_TIMEOUT = 5000;
+    //private static final String STRING_TO_BE_TYPED = "UiAutomator";
+    private UiDevice mDevice;
+    private final int SLOW=1000;
+    private final int FAST=10;
+    private final int VERYSLOW=5000;
+
+
+    @Test
+    public void Test1() throws InterruptedException, UiObjectNotFoundException, RemoteException {
+        int time=SLOW;
+
+        //Semaphore Declaration
+        DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Start = new Semaphore(1);
+        DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Finish = new Semaphore(1);
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Finish = new Semaphore(1);
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Start = new Semaphore(1);
+
+        //Set Semaphores
+        Log.d("TEST", "Il test prova ad acquisire i semafori");
+        DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Start.acquire();
+        DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Finish.acquire();
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Finish.acquire();
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Start.acquire();
+
+        // Initialize UiDevice instance and Start application
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        mDevice.pressHome();
+        final String launcherPackage = mDevice.getLauncherPackageName();
+        assertThat(launcherPackage, notNullValue());
+        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)),LAUNCH_TIMEOUT);
+        Context context = InstrumentationRegistry.getContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+        mDevice.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),LAUNCH_TIMEOUT * 100);
+
+
+        //Start Download
+        Thread.sleep(time);
+        DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Start.release();
+        Log.d("TEST", "TEST: Started Download Task");
+
+        //Click Sort
+        Thread.sleep(time);
+        (mDevice.findObject(new UiSelector().resourceId("mimecast.com.recruitment:id/sortButton"))).click();
+        Log.d("TEST", "TEST: Click Sort");
+
+        //Start Sort
+        Thread.sleep(time);
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Start.release();
+        Log.d("TEST", "TEST: Start Sort Task");
+
+        //pause
+        Thread.sleep(time);
+        mDevice.pressHome();
+
+        //Finish Download
+        Thread.sleep(time);
+        DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Finish.release();
+        Log.d("TEST", "TEST: Finish Sort Task");
+
+        //Finish Sort
+        Thread.sleep(time);
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Finish.release();
+        Log.d("TEST", "TEST: Finish Sort Task");
+
+        //resume
+        Thread.sleep(time);
+        mDevice.pressRecentApps();
+        Thread.sleep(time);
+        mDevice.pressRecentApps();
+
+
+        //Click Sort
+        Thread.sleep(time);
+        (mDevice.findObject(new UiSelector().resourceId("mimecast.com.recruitment:id/sortButton"))).click();
+        Log.d("TEST", "TEST: Click Sort");
+
+        //Start Sort
+        Thread.sleep(time);
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Start.release();
+        Log.d("TEST", "TEST: Start Sort Task");
+
+        //Finish Sort
+        Thread.sleep(time);
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Finish.release();
+        Log.d("TEST", "TEST: Finish Sort Task");
+    }
+
+    @Test
+    public void Test1Fast() throws InterruptedException, UiObjectNotFoundException, RemoteException {
+        int time=FAST;
+
+        //Semaphore Declaration
+        DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Start = new Semaphore(1);
+        DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Finish = new Semaphore(1);
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Finish = new Semaphore(1);
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Start = new Semaphore(1);
+
+        //Set Semaphores
+        Log.d("TEST", "Il test prova ad acquisire i semafori");
+        DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Start.acquire();
+        DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Finish.acquire();
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Finish.acquire();
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Start.acquire();
+
+        // Initialize UiDevice instance and Start application
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        mDevice.pressHome();
+        final String launcherPackage = mDevice.getLauncherPackageName();
+        assertThat(launcherPackage, notNullValue());
+        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)),LAUNCH_TIMEOUT);
+        Context context = InstrumentationRegistry.getContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+        mDevice.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),LAUNCH_TIMEOUT * 100);
+
+
+        //Start Download
+        Thread.sleep(time);
+        DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Start.release();
+        Log.d("TEST", "TEST: Started Download Task");
+
+        //Click Sort
+        Thread.sleep(time);
+        (mDevice.findObject(new UiSelector().resourceId("mimecast.com.recruitment:id/sortButton"))).click();
+        Log.d("TEST", "TEST: Click Sort");
+
+        //Start Sort
+        Thread.sleep(time);
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Start.release();
+        Log.d("TEST", "TEST: Start Sort Task");
+
+        //pause
+        Thread.sleep(time);
+        mDevice.pressHome();
+
+        //Finish Download
+        Thread.sleep(time);
+        DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Finish.release();
+        Log.d("TEST", "TEST: Finish Sort Task");
+
+        //Finish Sort
+        Thread.sleep(time);
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Finish.release();
+        Log.d("TEST", "TEST: Finish Sort Task");
+
+        //resume
+        Thread.sleep(time);
+        mDevice.pressRecentApps();
+        Thread.sleep(time);
+        mDevice.pressRecentApps();
+
+
+        //Click Sort
+        Thread.sleep(time);
+        (mDevice.findObject(new UiSelector().resourceId("mimecast.com.recruitment:id/sortButton"))).click();
+        Log.d("TEST", "TEST: Click Sort");
+
+        //Start Sort
+        Thread.sleep(time);
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Start.release();
+        Log.d("TEST", "TEST: Start Sort Task");
+
+        //Finish Sort
+        Thread.sleep(time);
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Finish.release();
+        Log.d("TEST", "TEST: Finish Sort Task");
+    }
+
+}
--- app/src/androidTest/java/mimecast/com/recruitment/UIAutomatorAsyncTaskTestZero.java
+++ app/src/androidTest/java/mimecast/com/recruitment/UIAutomatorAsyncTaskTestZero.java
@@ -1,4 +1,4 @@
-package mimecast.com.recruitment.application;
+package mimecast.com.recruitment;
 
 import android.content.Context;
 import android.content.Intent;
@@ -27,94 +27,84 @@ import static org.junit.Assert.assertThat;
 
 @RunWith(AndroidJUnit4.class)
 @SdkSuppress(minSdkVersion = 18)
-public class UIAutomatorAsyncTaskTest {
+public class UIAutomatorAsyncTaskTestZero {
 
     private static final String BASIC_SAMPLE_PACKAGE
             = "mimecast.com.recruitment";
     private static final int LAUNCH_TIMEOUT = 5000;
     private static final String STRING_TO_BE_TYPED = "UiAutomator";
     private UiDevice mDevice;
-
+    private final int SLOW=1000;
+    private final int FAST=10;
+    private final int VERYSLOW=5000;
 
     @Test
-    public void PrimoTest() throws InterruptedException, UiObjectNotFoundException, RemoteException {
-        // SEQUENZA UI->TASK1->TASK2->Sort
-
-
-        // Definisco i semafori, uno per ogni task, eventualmente settando il numero di task possibili
+    public void ZeroTest() throws InterruptedException, UiObjectNotFoundException, RemoteException {
+        int time=SLOW;
+        //Semaphore Declaration
         DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Start = new Semaphore(1);
         DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Finish = new Semaphore(1);
         EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Finish = new Semaphore(1);
         EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Start = new Semaphore(1);
 
-
-        //Il test mette rosso i semafori, in modo da poterne determinare autonomamente lo sblocco
+        //Set Semaphores
         Log.d("TEST", "Il test prova ad acquisire i semafori");
         DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Start.acquire();
         DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Finish.acquire();
         EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Finish.acquire();
         EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Start.acquire();
 
-        // Initialize UiDevice instance
+        // Initialize UiDevice instance and Start application
         mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-
-        // Start from the home screen
         mDevice.pressHome();
-
-        // Wait for launcher
         final String launcherPackage = mDevice.getLauncherPackageName();
         assertThat(launcherPackage, notNullValue());
-        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)),
-                LAUNCH_TIMEOUT);
-
-        // Launch the app
+        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)),LAUNCH_TIMEOUT);
         Context context = InstrumentationRegistry.getContext();
         final Intent intent = context.getPackageManager()
                 .getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE);
-        // Clear out any previous instances
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         context.startActivity(intent);
-
-        Log.d("TEST", "TEST: Avvio la activity");
-        //Log.d("TEST","TEST: valori dei semafori: Download="+DownloadMezziTask.taskDownload.availablePermits()+" meteo="+LeggiMeteoTask.taskMeteo.availablePermits());
-        //Thread.sleep(5000);
-        // Wait for the app to appear
-        mDevice.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
-                LAUNCH_TIMEOUT * 100);
-        Log.d("TEST", "TEST: Fine before");
-        //La app è stata avviata
+        mDevice.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),LAUNCH_TIMEOUT * 100);
 
 
-        Thread.sleep(2000);
-        Log.d("TEST", "TEST: Il test sblocca l'avvio del task meteo");
+        //Start Download
+        Thread.sleep(time);
         DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Start.release();
-        Log.d("TEST", "TEST: Rilasciato il semaforo meteo");
+        Log.d("TEST", "TEST: Started Download Task");
 
-        Thread.sleep(1000);
-        Log.d("TEST", "TEST: Il test sblocca la terminazione del task meteo");
+        //Finish Download
+        Thread.sleep(time);
         DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Finish.release();
-        //Log.d("TEST", "TEST: Rilasciato il semaforo meteo");
+        Log.d("TEST", "TEST: Finished Download Task");
 
-        Thread.sleep(2000);
-        Log.d("TEST", "TEST: Il test sblocca l'avvio del task download");
+        //Start Sort
+        Thread.sleep(time);
         EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Start.release();
-        //Log.d("TEST", "TEST: Rilasciato il semaforo download");
+        Log.d("TEST", "TEST: Start Sort Task");
 
-        Thread.sleep(1000);
-        Log.d("TEST", "TEST: Il test sblocca la terminazione del task download");
-        DownloadEmailHeaderListTask.task_DownloadEmailHeaderList_Finish.release();
+        //Finish Sort
+        Thread.sleep(time);
+        EmailHeaderListReverseChronologicalSortTask.task_EmailHeaderListReverseChronologicalSort_Finish.release();
+        Log.d("TEST", "TEST: Finish Sort Task");
 
-
-        UiObject ui = mDevice.findObject(new UiSelector().text("Sort"));
-        ui.click();
-
-        Thread.sleep(2000);
+        //pause
+        Thread.sleep(time);
         mDevice.pressHome();
-        Thread.sleep(2000);
+
+        //resume
+        Thread.sleep(time);
         mDevice.pressRecentApps();
-        Thread.sleep(1000);
+        Thread.sleep(time);
         mDevice.pressRecentApps();
-        Thread.sleep(2000);
+
+        //Click Sort
+        Thread.sleep(time);
+        (mDevice.findObject(new UiSelector().resourceId("mimecast.com.recruitment:id/sortButton"))).click();
+        Log.d("TEST", "TEST: Click Sort");
+
+
+
     }
 
 }
